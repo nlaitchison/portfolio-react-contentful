4025155dd9c3f2007f2fd6f9b82c5a7fee279164
Nicole Aitchison <nicole.aitchison@Nicoles-MacBook-Pro.local>
2017-03-09 20:34:45 -0500

    first commit

---
+++ config.js
@@ -0,0 +1,4 @@
+export const deliveryAccessToken = 'a45653ee0d288bf17e5785159f96156242df00d0273fc0ff098a862f359b569c';
+export const spaceId = '90eku9h02cpz';
+
+export const galleryTypeId = '7leLzv8hW06amGmke86y8G';
